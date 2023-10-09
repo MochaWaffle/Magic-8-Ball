9707f06e001dc3dfab503fb1759ddbdc4236150b
MochaWaffle <vsaini23@medford.k12.ma.us>
2023-10-09 16:55:41 -0400

    Centered 8Ball and input fields
    Centered 8ball and input fields, made it fully responsive, and changed background color, and added box-shadow to the 8Ball.

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const response = ["No", "Yes", "Maybe", "Reflect and try again later", "I cannot
                     "Perhaps", "You probably should", "You probably should not", "Sure?", "I don't think that's a good idea"];
 
 let defaultContent = document.getElementById("defaultContent");
+
 function shake() {
     var inputField = document.getElementById("input");
-    let randomIndex = Math.floor(Math.random() * response.length)
+    let randomIndex = Math.floor(Math.random() * response.length);
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
     message = response[randomIndex];
 
     inputField.value = "";
     defaultContent.textContent = message;
-    console.log(message);
+    //console.log(message);
+    console.log("Screen Width: " + screenWidth);
+    console.log("Screen Height: " + screenHeight);
 }
