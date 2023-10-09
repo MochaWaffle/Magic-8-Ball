e24a0068ce66167518102871ca92c77522bc0d55
MochaWaffle <vsaini23@medford.k12.ma.us>
2023-10-08 20:49:29 -0400

    input changes 8ball text
    When a user enters a message in the input field, the magic 8 ball gives a response. The input field is required, so if you don't type anything, the enter button will not shake the 8 ball, meaning you won't get a response.

---
+++ script.js
@@ -0,0 +1,14 @@
+let message = "";
+const response = ["No", "Yes", "Maybe", "Reflect and try again later", "I cannot answer that, sorry", "Do as your heart says", 
+                    "Perhaps", "You probably should", "You probably should not", "Sure?", "I don't think that's a good idea"];
+
+let defaultContent = document.getElementById("defaultContent");
+function shake() {
+    var inputField = document.getElementById("input");
+    let randomIndex = Math.floor(Math.random() * response.length)
+    message = response[randomIndex];
+
+    inputField.value = "";
+    defaultContent.textContent = message;
+    console.log(message);
+}
