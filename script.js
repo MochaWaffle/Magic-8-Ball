d89e3888aa336fc77bc42e64f89ea5814c19fbeb
MochaWaffle <vsaini23@medford.k12.ma.us>
2023-10-09 17:40:23 -0400

    Made 8Ball text responsive
    The 8 Ball response text is now responsive where it fits inside the white circle, and does not go outside of it.

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let message = "";
 const response = ["No", "Yes", "Maybe", "Reflect and try again later", "I cannot answer that, sorry", "Do as your heart says", 
                     "Perhaps", "You probably should", "You probably should not", "Sure?", "I don't think that's a good idea"];
 
-let defaultContent = document.getElementById("defaultContent");
+let eightBallText = document.getElementById("eightBallText");
+const contentContainer = document.getElementById("contentContainer");
 
 function shake() {
     var inputField = document.getElementById("input");
@@ -12,8 +13,28 @@ function shake() {
     message = response[randomIndex];
 
     inputField.value = "";
-    defaultContent.textContent = message;
+    eightBallText.textContent = message;
+    fitText();
     //console.log(message);
     console.log("Screen Width: " + screenWidth);
     console.log("Screen Height: " + screenHeight);
 }
+
+function fitText() {
+    const contentWidth = contentContainer.clientWidth - 3;
+    const contentHeight = contentContainer.clientHeight - 3;
+
+    eightBallText.style.fontSize = "100px";
+
+    while (eightBallText.clientWidth >= contentWidth || eightBallText.clientHeight >= contentHeight) {
+        const currentSize = parseFloat(getComputedStyle(eightBallText).fontSize);
+        eightBallText.style.fontSize = (currentSize - 1) + "px";
+    }
+    console.log("message: ", message);
+    console.log("container width: ", contentWidth);
+    console.log("text width: ", eightBallText.clientWidth);
+}
+
+window.addEventListener("resize", fitText);
+
+window.addEventListener("load", fitText);
