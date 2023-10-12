ededfa9cf187982112fbf7c5f767c3ba8d7db45c
MochaWaffle <vsaini23@medford.k12.ma.us>
2023-10-11 22:27:40 -0400

    Organized JavaScript
    Deleted commented codes, and organized JavaScript code more.

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 const eightBallContainer = document.getElementById("eightBallContainer");
-const contentContainer = document.getElementById("contentContainer");
-const colorBox = document.getElementById("contentContainer");
-const root = document.documentElement;
+const colorCircle = document.getElementById("contentContainer");
 const submitButton = document.getElementById("submitButton");
-
-let currentScale = 1;
-let currentRotation = 0;
-let currentFontSize = 18;
+const root = document.documentElement;
 const targetScale = 1.2;
 const targetRotation = 5;
 const targetFontSize = 25;
-let keyframesStyle = document.styleSheets[0].insertRule(`@keyframes buttonHoverAnimation { 
-    0% { transform: scale(${currentScale}) rotate(${currentRotation}deg); font-size: ${currentFontSize}px; } 
-    100% { transform: scale(${targetScale}) rotate(${targetRotation}deg); font-size: ${targetFontSize}px; } 
-}`, 0);
-let keyframesStyle2 = document.styleSheets[1].insertRule(`@keyframes buttonNotHoverAnimation { 
-    0% { transform: scale(${currentScale}) rotate(${currentRotation}deg); font-size: ${currentFontSize}px; } 
-    100% { transform: scale(1) rotate(0deg); font-size: 18px; } 
-}`, 1);
 const response = {
     "No": "No",
     "My records say no": "No", 
@@ -36,8 +23,20 @@ const response = {
     "Do as your heart says": "Unsure", 
     "I honestly don't know": "Unsure",
 };
+
 let message = "";
 let eightBallText = document.getElementById("eightBallText");
+let currentScale = 1;
+let currentRotation = 0;
+let currentFontSize = 18;
+let keyframesStyle = document.styleSheets[0].insertRule(`@keyframes buttonHoverAnimation { 
+    0% { transform: scale(${currentScale}) rotate(${currentRotation}deg); font-size: ${currentFontSize}px; } 
+    100% { transform: scale(${targetScale}) rotate(${targetRotation}deg); font-size: ${targetFontSize}px; } 
+}`, 0);
+let keyframesStyle2 = document.styleSheets[1].insertRule(`@keyframes buttonNotHoverAnimation { 
+    0% { transform: scale(${currentScale}) rotate(${currentRotation}deg); font-size: ${currentFontSize}px; } 
+    100% { transform: scale(1) rotate(0deg); font-size: 18px; } 
+}`, 1);
 
 function shake() {
     var inputField = document.getElementById("input");
@@ -47,7 +46,7 @@ function shake() {
     inputField.value = "";
     eightBallText.textContent = "8";
 
-    colorBox.style.backgroundColor = `rgb(255, 255, 255)`;
+    colorCircle.style.backgroundColor = `rgb(255, 255, 255)`;
     root.style.setProperty("--outlineBoxShadowColor2", "0 0 50px #3d0982");
     root.style.setProperty("--outlineBoxShadowColor3", "0 0 450px #bb36d5");
 
@@ -57,8 +56,8 @@ function shake() {
 }
 
 function fitText() {
-    const contentWidth = contentContainer.clientWidth - 3;
-    const contentHeight = contentContainer.clientHeight - 3;
+    const contentWidth = colorCircle.clientWidth - 3;
+    const contentHeight = colorCircle.clientHeight - 3;
 
     eightBallText.style.fontSize = "100px";
 
@@ -79,6 +78,7 @@ function startAnimations() {
 
 function startColorFade() {
     const step = 10;
+    
     let currentColor = 255;
     let currentBoxShadowOneR = 61;
     let currentBoxShadowOneG = 9;
@@ -86,14 +86,13 @@ function startColorFade() {
     let currentBoxShadowTwoR = 187;
     let currentBoxShadowTwoG = 54;
     let currentBoxShadowTwoB = 213;
-    //let currentsBoxShadow = [currentBoxShadowOneR, currentBoxShadowOneG, currentBoxShadowOneB, currentBoxShadowTwoR, currentBoxShadowTwoG, currentBoxShadowTwoB];
+
     let targetColor = 0;
-    //let rgbString = ``;
 
     const interval = setInterval(function() {
         if (currentColor > targetColor) {
             currentColor -= step;
-            colorBox.style.backgroundColor = `rgb(${currentColor}, ${currentColor}, ${currentColor})`;
+            colorCircle.style.backgroundColor = `rgb(${currentColor}, ${currentColor}, ${currentColor})`;
             eightBallText.textContent = "";
 
             if (currentBoxShadowOneR > targetColor) {
@@ -114,11 +113,7 @@ function startColorFade() {
             if (currentBoxShadowTwoB > targetColor) {
                 currentBoxShadowTwoB -= step;
             }
-            // for (i = 0; i < currentsBoxShadow.length; i++) {
-            //     if (currentsBoxShadow[i] > targetColor) {
-            //         currentsBoxShadow[i] -= step;
-            //     }
-            // }
+
             root.style.setProperty("--outlineBoxShadowColor2", `0 0 50px rgb(${currentBoxShadowOneR}, ${currentBoxShadowOneG}, ${currentBoxShadowOneB})`);
             root.style.setProperty("--outlineBoxShadowColor3", `0 0 450px rgb(${currentBoxShadowTwoR}, ${currentBoxShadowTwoG}, ${currentBoxShadowTwoB})`);
         } else {
@@ -134,26 +129,29 @@ function startColorFade() {
 
 function startColorFade2() {
     const step = 5;
+
     let currentR = 0;
     let currentG = 0;
     let currentB = 0;
+    
     let currentBoxShadowOneR = 0;
     let currentBoxShadowOneG = 0;
     let currentBoxShadowOneB = 0;
     let currentBoxShadowTwoR = 0;
     let currentBoxShadowTwoG = 0;
     let currentBoxShadowTwoB = 0;
+
     let targetR = 255;
     let targetG = 255;
     let targetB = 255;
+
     let targetBoxShadowOneR = 0;
     let targetBoxShadowOneG = 0;
     let targetBoxShadowOneB = 0;
     let targetBoxShadowTwoR = 0;
     let targetBoxShadowTwoG = 0;
     let targetBoxShadowTwoB = 0;
-    //let currents = [currentR, currentG, currentB, currentBoxShadowOneR, currentBoxShadowOneG, currentBoxShadowOneB, currentBoxShadowTwoR, currentBoxShadowTwoG, currentBoxShadowTwoB];
-    //slet targets = [targetR, targetG, targetB, targetBoxShadowOneR, targetBoxShadowOneG, targetBoxShadowOneB, targetBoxShadowTwoR, targetBoxShadowTwoG, targetBoxShadowTwoB];
+
     if (response[message] === "No") {
         targetR = 255;
         targetG = 0;
@@ -224,12 +222,8 @@ function startColorFade2() {
             if (currentBoxShadowTwoB < targetBoxShadowTwoB) {
                 currentBoxShadowTwoB += step;
             }
-            // for (i = 0; i < currents.length; i++) {
-            //     if (currents[i] < targets[i]) {
-            //         currents[i] += step;
-            //     }
-            // }
-            colorBox.style.backgroundColor = `rgb(${currentR}, ${currentG}, ${currentB})`;
+
+            colorCircle.style.backgroundColor = `rgb(${currentR}, ${currentG}, ${currentB})`;
             root.style.setProperty("--outlineBoxShadowColor2", `0 0 50px rgb(${currentBoxShadowOneR}, ${currentBoxShadowOneG}, ${currentBoxShadowOneB})`);
             root.style.setProperty("--outlineBoxShadowColor3", `0 0 450px rgb(${currentBoxShadowTwoR}, ${currentBoxShadowTwoG}, ${currentBoxShadowTwoB})`);
         } else {
@@ -270,7 +264,6 @@ submitButton.addEventListener("mouseout", () => {
         100% { transform: scale(1) rotate(0deg); font-size: 18px; } 
     }`, 1);
     submitButton.style.animation = "buttonNotHoverAnimation 0.3s ease forwards";
-    // submitButton.style.animationDelay = "0s";
 })
 function getCurrentScale(element) {
     const transform = window.getComputedStyle(element, null).getPropertyValue("transform");
